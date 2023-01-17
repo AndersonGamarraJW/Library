1f1dbfbb4f3f33f6959884286b73d38ecc814192
AndersonGamarraJW <andersong.v3105@gmail.com>
2023-01-17 03:21:37 -0500

    Vincula los archivos

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ function Book(title,author,numeroPaginas,isRead){
     this.isRead = isRead;
     
 }
-function addToLibrary(){
-    
+Book.prototype.printInfo = function (){
+    console.log('Title:',this.title);
+    console.log('Author:',this.author);
+    console.log('Number Pages:',this.numeroPaginas);
+    console.log('Is read',this.isRead);
 }
-book1 = Book('La Odisea','Homero',43,true);
+function addToLibrary(book){
+    books.push(book);
+}
+const book1 = new Book('La Odisea','Homero',43,true);
+addToLibrary(book1);
+book1.printInfo();
