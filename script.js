fe8190f703f6f2a3283d57aa56da5396fe806718
AndersonGamarraJW <andersong.v3105@gmail.com>
2023-01-17 03:10:52 -0500

    feat: Implementa la clase Book

---
+++ script.js
@@ -0,0 +1,13 @@
+let books = [];
+
+function Book(title,author,numeroPaginas,isRead){
+    this.title = title;
+    this.author = author;
+    this.numeroPaginas = numeroPaginas;
+    this.isRead = isRead;
+    
+}
+function addToLibrary(){
+    
+}
+book1 = Book('La Odisea','Homero',43,true);
